787b28ebb4c33e2724f8820085c2ede9748d7773
navjot <mail.navjot77@gmail.com>
2016-09-02 13:24:23 -0700

    Added NYTIMES ajax api

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+// ea39d98ed26c40bf8c5acde1d9f3cec0 API for NY times
 function loadData() {
 
     var $body = $('body');
@@ -14,6 +15,37 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+    $greeting.text("Coool");
+    var street_address = $('#street').val();
+    var city = $('#city').val();
+    var streetAdress='<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x400&location=%data%">';
+    var imgAddress=streetAdress.replace("%data%",street_address+' , '+city);
+    console.log(imgAddress);
+    $body.append(imgAddress);
+
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({'api-key': "ea39d98ed26c40bf8c5acde1d9f3cec0", 'sort': "newest",
+        'q':city});
+    $.ajax({url: url,method: 'GET',}).done(function(data) {
+          console.log(data);
+        $nytElem.text("");
+        articles=data.response.docs;
+            for (i=0; i<articles.length; i++) {
+                article=articles[i]
+                if (typeof article.headline.print_headline == "undefined")
+                    console.log("Its undfined");
+                else
+                $nytElem.append("<li>" + article.headline.print_headline+ "</li>");
+            }
+
+
+    }).error(function(err){
+        console.log(err);
+        $nytElem.append("<h1>Unable to load tghe page </h2>");
+
+    });
+
+
 
     return false;
 };
